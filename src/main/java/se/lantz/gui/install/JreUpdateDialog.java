f4ebfb5d62650ad5301cf9fd0174b53a644a9fe5
lantzelot-swe <75668734+lantzelot-swe@users.noreply.github.com>
2024-10-04 23:51:13 +0200

    small fix in the update dialog

--- src/main/java/se/lantz/gui/install/JreUpdateDialog.java
+++ src/main/java/se/lantz/gui/install/JreUpdateDialog.java
@@ -25,10 +25,10 @@ public class JreUpdateDialog extends BaseDialog
     if (panel == null)
     {
       String downloadUrl = "https://github.com/lantzelot-swe/PCUGameManager/releases/tag/3.1.0";
-      String jreUrl = "https://github.com/lantzelot-swe/PCUGameManager/releases/tag/3.1.0/jre18.zip";
+      String jreUrl = "https://github.com/lantzelot-swe/PCUGameManager/releases/tag/3.1.0/xjre18.zip";
       String message =
         "<html>This version of PCUAE Manager requires an updated JRE. Follow these steps to update:<ol><li>Go to <a href='" +
-          downloadUrl + "'>Release 3.1.0</a>" + " and download <a href='" + jreUrl + "'>jre18.zip</a>.</li>" +
+          downloadUrl + "'>Release 3.1.0</a>" + " and download <a href='" + jreUrl + "'>xjre18.zip</a>.</li>" +
           "<li>Exit this program by closing this dialog.</li>" +
           "<li>Delete the \"jre\" folder in the PCUAE Manager main directory.</li>" +
           "<li>Unpack the zip file in the PCUAE Manager main directory, replacing the \"jre\" folder. An additonal folder named \"natives\" is also created.</li>" +
